0142b19d1320d45da7c8a22eba0ac1e30d4a16dd
user <user@DESKTOP-CEJKNMA>
2021-03-10 23:25:13 -0600

    primer commit

---
+++ src/ejercicio/pkg2/Ejercicio2.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg2;
+
+/**
+ *
+ * @author user
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       double n1=2, n2=3, n3=9,n4=5;
+        double divi, mul, sum, rest;
+    
+     System.out.println("__________________________________________________________\n");
+          System.out.println("suma\n");
+          sum=n1+n2;
+          System.out.println(n1+ " + "+ n2 + " = " + sum+"\n");
+          sum=n3+n4;
+            System.out.println(n3+ " + "+ n4 + " = " + sum +"\n");
+ System.out.println("__________________________________________________________\n");
+                    System.out.println("Resta\n");
+          rest=n1-n2;
+          System.out.println(n1+ " - "+ n2 + "=" + rest+"\n");
+          rest=n3-n4;
+            System.out.println(n3+ " - "+ n4 + "=" + rest +"\n");
+        System.out.println("__________________________________________________________\n");   
+                        System.out.println("multiplicacion\n");
+      
+                        mul=n1*n2;
+          System.out.println(n1+ " x "+ n2 + " = " + mul+"\n");
+          mul=n3*n4;
+            System.out.println(n3+ " x "+ n4 + " = " + mul+"\n");
+     System.out.println("__________________________________________________________\n");
+                System.out.println("divison\n");
+                       divi=n1/n2;
+          System.out.println(n1+ " / "+ n2 + " = " + divi+"\n");
+          divi=n3/n4;
+            System.out.println(n3+ " / "+ n4 + " = " + divi+"\n");
+            
+    }
+    
+}
